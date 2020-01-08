3b41dffcfa63af810f08294de09d9482f3369748
mt-elektrik <muarateknikelektrik@gmail.com>
2020-01-08 23:54:02 +0700

    add event onActive,onRelease

--- examples/InputEventExample.ino
+++ examples/InputEventExample.ino
@@ -1,7 +1,15 @@
 #include "MteCore.h"
+void IN1Aktif(){
+    Serial.println("Oke");
+}
+void In1NonAktif(){
+    Serial.println("Kaga Aktif");
+}
 void setup(){
-
+    Serial.begin(9600);
+    IN1.onActive(IN1Aktif);
+    IN1.onRelease(In1NonAktif);
 }
 void loop(){
-
-}
+    process();
+}   
--- src/InputEvent.h
+++ src/InputEvent.h
@@ -1,45 +1,89 @@
 // #ifdef _MTE_CORE_H
-#ifndef _INPUT_EVENT_H
-#define _INPUT_EVENT_H
-
+#ifndef _INPUT_EVENT_H 
+#define _INPUT_EVENT_H 
 #include "Arduino.h"
-typedef void (*inputevent_callback_active);
-class InputEvent
-{
-private:
-    int8_t _pinIn;
-    inputevent_callback_active _cb_Active;
-public:
-    InputEvent(int8_t pinIn);
-    // ~InputEvent();
-    bool isActive();
-    void onActive(inputevent_callback_active cb);
-    void onActiveDuration(unsigned long timerMS);
 
-    void process();
+
+typedef void(*inputEvent_State_Callback)();
+
+class InputEvent {
+    protected:
+    private:
+        bool firstState;
+        InputEvent(const InputEvent&);
+        InputEvent& operator=(const InputEvent&);
+        const byte _defaultDelay = 20;
+        unsigned long lastDebounceTime;
+        bool lastButtonState=HIGH;
+        uint8_t _pinIn;
+        inputEvent_State_Callback _cba;
+        inputEvent_State_Callback _cbna;
+    public:
+        explicit InputEvent(uint8_t pinIn);
+        virtual~InputEvent();
+        void onActive(inputEvent_State_Callback cb);
+        void onRelease(inputEvent_State_Callback cb);
+        bool isActive();
+        void process();
 };
+InputEvent::InputEvent(uint8_t pinIn) {
+    _pinIn=pinIn;
+}
+
+InputEvent::~InputEvent() {}
+
+void InputEvent::onActive(inputEvent_State_Callback cb) {
+    _cba=cb;
+}
+void InputEvent::onRelease(inputEvent_State_Callback cb ){
+    _cbna=cb;
+}
+bool InputEvent::isActive(){
+    return !digitalRead(_pinIn);
+}
+void InputEvent::process(){
+  int reading = digitalRead(_pinIn);
+  if (millis() - lastDebounceTime > _defaultDelay) {
+        if(reading!=lastButtonState){
+            firstState=true;
+            if(reading==LOW){
+                if(_cba){
+                    _cba();
+                }
+            }else{
+                if(_cbna){
+                    _cbna();
+                }
+            }
+        }else{
+            if(!firstState){
+                firstState=true;
+                if(reading==LOW){
+                    if(_cba){
+                        _cba();
+                    }
+                }else{
+                    if(_cbna){
+                        _cbna();
+                    }
+                }
+            }
+        }
+    lastButtonState = reading;
+    lastDebounceTime = millis();  
+  }
+//   if (reading != lastButtonState) {
+//     lastDebounceTime = millis();
+//   }
+//   if (millis() - lastDebounceTime > 20) {
+//     // if(reading == LOW){
+//     //     if(_cba){
+//     //         _cba;
+//     //     }
+//     // }
+//     Serial.println(reading);
+//   }
+//   lastButtonState = reading;
+}
 #endif //_INPUT_EVENT_H
 // #endif //_MTE_CORE_H
-
-InputEvent::InputEvent(int8_t pinIn)
-{
-    _pinIn = pinIn;
-    pinMode(pinIn,INPUT_PULLUP);
-}
-
-// InputEvent::~InputEvent()
-// {
-// }
-InputEvent::isActive(){
-    return digitalRead(_pinIn);
-}
-InputEvent::onActive(inputevent_callback_active cb){
-    _cb_Active = cb;
-}
-InputEvent::onActiveDuration(unsigned long timerMS){
-
-}
-InputEvent::process(){
-    unsigned long now = millis();
-    
-}
